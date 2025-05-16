13630b6f8f796ea5d18bb2fea53822c0c6dcca00
MUHAMMAD <muhammadbinabubakr@gmail.com>
2025-05-16 06:40:19 +0300

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="text-3xl font-bold">About us page</h2>
+    <p class="mt-4 text-lg text-gray-600">My name is Muhammad !</p>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="text-3xl font-bold">Contact us</h2>
+    <p class="mt-4 text-lg text-gray-600">Email: whatever@gmail.com</p>
+@endsection
